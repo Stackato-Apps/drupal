9a29563db04b5eaeedd0202e0fd9be356dba52c8
Steven Hsu <stevenhsu0@gmail.com>
2012-10-30 11:41:00 -0700

    Modified/fixed custom distribution setup

--- custom-profile.sh
+++ custom-profile.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # PROFILE URL (no core)
-PROFILE_URL=http://ftp.drupal.org/files/projects/commerce_kickstart-7.x-2.0-rc2-no-core.tar.gz
+PROFILE_URL=http://ftp.drupal.org/files/projects/commerce_kickstart-7.x-1.11-no-core.tar.gz
 
 # Then we get drupal distro files (no-core) from drupal.org
 curl -sfS $PROFILE_URL | tar xzf - -C profiles
-cp -r profiles/commerce_kickstart/modules/* sites/all/modules/
-rm -rf profiles/commerce_kickstart/modules/*
+#cp -r profiles/commerce_kickstart/modules/* sites/all/modules/
+#rm -rf profiles/commerce_kickstart/modules/*
 
-if ! [ -e $FS/INSTALLED ]
+if ! [ -e $STACKATO_FILESYSTEM/INSTALLED ]
   then
     echo "Installing Commerce Kickstart Drupal..."
 
     $STACKATO_APP_ROOT/drush/drush -r $HOME site-install commerce_kickstart -y --db-url=$DATABASE_URL --account-name=admin --account-pass=passwd --site-name=Stackato --locale=en-US
 
     # Drupal successfully installed
-    touch $FS/INSTALLED
+    touch $STACKATO_FILESYSTEM/INSTALLED
 fi
