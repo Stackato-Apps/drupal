a266641ef37738eb74a734ad412170949664a17d
Billy Tat <billyt@activestate.com>
2014-07-14 13:43:42 -0700

    Add stackato-sso-workaround.sh to modify includes/bootstrap.inc after Drupal is installed so is_https is set when Stackato sso is enabled

---
+++ stackato-sso-workaround.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+file="includes/bootstrap.inc"
+line="$(sed -n '/is_https =/=' $file)"
+
+# remove line that sets is_https
+sed -i "$line"d $file
+
+# get value of sso_enabled from VCAP_APPLICATION environment variable
+# add line back that sets is_https with additional check to see if sso is enabled
+sed -i "$line"i"\$vcap_app = json_decode(getenv('VCAP_APPLICATION'), true);" $file
+sed -i "$((line+1))"i"\$sso_enabled = \$vcap_app['sso_enabled'];" $file
+sed -i "$((line+2))"i"\$is_https = \$sso_enabled || (isset(\$_SERVER['HTTPS']) && strtolower(\$_SERVER['HTTPS']) == 'on');" $file
