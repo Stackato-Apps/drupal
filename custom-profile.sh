8285d488bc259c7a126ba9d686c7ea20019e8795
Billy Tat <billyt@activestate.com>
2014-08-15 13:46:08 -0700

    Use non-legacy buildpack

--- custom-profile.sh
+++ custom-profile.sh
@@ -4,7 +4,7 @@
 PROFILE_URL=http://ftp.drupal.org/files/projects/commerce_kickstart-7.x-1.11-no-core.tar.gz
 
 # Then we get drupal distro files (no-core) from drupal.org
-curl -sfS $PROFILE_URL | tar xzf - -C profiles
+curl -sfS $PROFILE_URL | tar xzf - -C www/profiles
 #cp -r profiles/commerce_kickstart/modules/* sites/all/modules/
 #rm -rf profiles/commerce_kickstart/modules/*
 
@@ -12,7 +12,7 @@ if ! [ -e $STACKATO_FILESYSTEM/INSTALLED ]
   then
     echo "Installing Commerce Kickstart Drupal..."
 
-    $STACKATO_APP_ROOT/drush/drush -r $HOME site-install commerce_kickstart -y --db-url=$DATABASE_URL --account-name=admin --account-pass=passwd --site-name=Stackato --locale=en-US
+    $STACKATO_APP_ROOT/drush/drush -r $HOME/www site-install commerce_kickstart -y --db-url=$DATABASE_URL --account-name=admin --account-pass=passwd --site-name=Stackato --locale=en-US
 
     # Drupal successfully installed
     touch $STACKATO_FILESYSTEM/INSTALLED
--- stackato-sso-workaround.sh
+++ stackato-sso-workaround.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-file="includes/bootstrap.inc"
+file=`dirname $0`"/includes/bootstrap.inc"
 line="$(sed -n '/is_https =/=' $file)"
 
 # remove line that sets is_https
